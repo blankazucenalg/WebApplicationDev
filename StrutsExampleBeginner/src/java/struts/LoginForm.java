13dfedd03a33dc20ce1625b40d449eaaf5aee3bf
Blanca Azucena Lopez <blankazucenalg@gmail.com>
2015-02-19 16:15:12 -0600

    Struts Login example!

---
+++ StrutsExampleBeginner/src/java/struts/LoginForm.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package struts;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+/**
+ *
+ * @author azu
+ */
+public class LoginForm extends org.apache.struts.action.ActionForm {
+
+    private String name;
+    private String email;
+    // error message
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError() {
+        this.error = "<span style='color:red'>Please provide valid entries for both fields</span>";
+    }
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param string
+     */
+    public void setName(String string) {
+        name = string;
+    }
+
+    /**
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param string
+     */
+    public void setEmail(String string) {
+        email = string;
+    }
+
+    /**
+     *
+     */
+    public LoginForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    @Override
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (getName() == null || getName().length() < 1) {
+            errors.add("name", new ActionMessage("error.name.required"));
+            // TODO: add 'error.name.required' key to your resources
+        }
+        return errors;
+    }
+}
